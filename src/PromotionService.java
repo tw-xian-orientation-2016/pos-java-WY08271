4473c57d0d397c30723652caeb7c547f020fdab1
WY08271 <wy08271@outlook.com>
2016-03-06 19:16:18 +0800

    modify PromotionService.java

--- src/PromotionService.java
+++ src/PromotionService.java
@@ -2,15 +2,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PromotionService {
-    private List<Promotion> promotions;
-
-    public void PromotionService(){
-        this.promotions = Arrays.asList(new Promotion("ITEM000000", "BUY_TWO_GET_ONE_FREE"),
-                new Promotion("ITEM000001", "BUY_TWO_GET_ONE_FREE"),
-                new Promotion("ITEM000005", "BUY_TWO_GET_ONE_FREE"));
-    }
+    private List<Promotion> promotions = Arrays.asList(new Promotion("ITEM000000", "BUY_TWO_GET_ONE_FREE"),
+            new Promotion("ITEM000001", "BUY_TWO_GET_ONE_FREE"),
+            new Promotion("ITEM000005", "BUY_TWO_GET_ONE_FREE"));
 
     public List<Promotion> getPromotions() {
-        return promotions;
+        return this.promotions;
     }
 }

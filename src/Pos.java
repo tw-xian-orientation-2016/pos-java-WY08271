494308614b40f9678822801941d9902e60985907
WY08271 <wy08271@outlook.com>
2016-03-06 16:40:24 +0800

    add findItem()

--- src/Pos.java
+++ src/Pos.java
@@ -1,4 +1,14 @@
+import java.util.List;
+import java.util.Optional;
+
 public class Pos {
+    private List<Item> items;
+    private List<Promotion> promotions;
+
+    public Pos(ItemService itemService, PromotionService promotionService){
+        this.items = itemService.getItems();
+        this.promotions = promotionService.getPromotions();
+    }
 
     public BarcodeCount scan(String tag) {
         String tagArray[] = tag.split("-");
@@ -6,6 +16,15 @@ public class Pos {
         float count = tagArray.length > 1 ? Float.parseFloat(tagArray[1]) : 1;
 
         return new BarcodeCount(tagArray[0], count);
+    }
+
+    public Item findItem(String barcode) {
+
+        Optional<Item> item = this.items.stream()
+                .filter(i -> i.getBarcode().equals(barcode))
+                .findFirst();
+
+        return item.isPresent() ? item.get() : null;
     }
 
 }

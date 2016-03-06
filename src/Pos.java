91f500781ac44cb1d4c68a0c759ae46bb489f832
WY08271 <wy08271@outlook.com>
2016-03-06 19:22:19 +0800

    delete unnecessary line

--- src/Pos.java
+++ src/Pos.java
@@ -18,7 +18,6 @@ public class Pos {
 
     public BarcodeCount scan(String tag) {
         String tagArray[] = tag.split("-");
-
         float count = tagArray.length > 1 ? Float.parseFloat(tagArray[1]) : 1;
 
         return new BarcodeCount(tagArray[0], count);
